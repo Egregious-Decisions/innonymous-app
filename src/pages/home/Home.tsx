a9eaedc3bcda737659fd47c1018430a2b1f1c7b5
Vladislav Safonov <29694249+ntdesmond@users.noreply.github.com>
2023-05-01 17:00:58 +0300

    Initial commit

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,28 @@
+import Header from '../../components/layout/Header';
+import PageBody from '../../components/layout/PageBody';
+import Main from '../../components/layout/Main';
+import MainMessage from './components/MainMessage';
+import AnimatedIcon from './components/AnimatedIcon';
+import Stack from '../../components/Stack';
+import SmallViteIcon from './components/SmallViteIcon';
+
+const Home = () => (
+  <PageBody>
+    <Header>Web application template</Header>
+    <Main>
+      <MainMessage>Hello world!</MainMessage>
+      <AnimatedIcon>
+        <Stack>
+          <div>🐳</div>
+          <SmallViteIcon />
+        </Stack>
+      </AnimatedIcon>
+    </Main>
+    <footer>
+      Source code available at{' '}
+      <a href="https://github.com/ntdesmond/styled-react-ts-vite">GitHub</a>!
+    </footer>
+  </PageBody>
+);
+
+export default Home;
