7b7fb68be1dc3fadc525446138256dec398b7a5a
Vladislav Safonov <29694249+ntdesmond@users.noreply.github.com>
2023-05-08 14:33:38 +0300

    Fetch user data in menu

--- src/pages/chats/components/menu/Menu.tsx
+++ src/pages/chats/components/menu/Menu.tsx
@@ -1,21 +1,40 @@
-import { BoxProps, Button, Flex, Icon, Spacer, Text } from '@chakra-ui/react';
+import {
+  BoxProps,
+  Button,
+  Flex,
+  Icon,
+  IconButton,
+  SkeletonText,
+  Spacer,
+  Text,
+} from '@chakra-ui/react';
 import { FaUserAlt } from 'react-icons/fa';
 import Header from '../Header';
 import ChatList from './ChatList';
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
 import { useAppDispatch } from '../../../../store/store';
 import { authSlice } from '../../../login/authSlice';
+import { apiSlice } from '../../../../store/apiSlice';
 
 const Menu = (props: BoxProps) => {
   const dispatch = useAppDispatch();
+  const { isFetching, data } = apiSlice.useGetCurrentUserQuery();
 
   const onLogOut = useCallback(() => dispatch(authSlice.actions.clearTokens()), [dispatch]);
+
+  const username = useMemo(() => {
+    if (isFetching || data === undefined) {
+      return <SkeletonText>Username</SkeletonText>;
+    }
+
+    return <Text>{data.name || data.alias}</Text>;
+  }, [data, isFetching]);
 
   return (
     <Flex flex="1" minHeight="0" direction="column" alignItems="stretch" {...props}>
       <Header>
         <Icon as={FaUserAlt} />
-        <Text>Username</Text>
+        {username}
         <Spacer />
         <Button onClick={onLogOut} colorScheme="red">
           log out
